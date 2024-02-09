5314007652c2b1ab0774b54ed314e18b1a15b341
Richard <richardmassengale@yaho.com>
2024-02-09 11:47:30 -0700

    finished coding challenge

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -53,4 +53,34 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+*/
+
+// 1. Select the element with the 'again' class and attach a click event handler
+document.querySelector('.again').addEventListener('click', function () {
+    // 2. In the handler function, restore initial values of the score and secretNumber variables
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+
+    // 3. Restore the initial conditions of the message, number, score and guess input field
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.guess').value = '';
+
+    // 4. Also restore the original background color (#222) and number width (15rem)
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
